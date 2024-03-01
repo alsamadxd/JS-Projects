7b20ca80fe62788a8019e102623f634e4700c09d
unknown <alsamad0005@gmail.com>
2024-03-01 20:47:33 +0530

    initial commit

---
+++ Weather App/index.js
@@ -0,0 +1,8 @@
+// const options = { method: "GET", headers: { accept: "application/json" } };
+
+// fetch(
+//   "https://api.tomorrow.io/v4/weather/realtime?location=delhi&apikey=fQ9As07fHu2ngwdoUAUyYdrWKVT9c16T"
+// )
+//   .then((response) => response.json())
+//   .then((response) => console.log(response.data.values.temperature))
+//   .catch((err) => console.error(err));
