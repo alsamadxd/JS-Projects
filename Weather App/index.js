794272e017e395e8fe29ba067bceec79731bce6a
unknown <alsamad0005@gmail.com>
2024-03-03 18:02:14 +0530

    Weather condition added

--- Weather App/index.js
+++ Weather App/index.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const temperature = document.querySelector(".temperature").value;
+let imageWeather = document.querySelector(".img");
 
 // const options = { method: "GET", headers: { accept: "application/json" } };
 
@@ -25,18 +26,44 @@ function getData(city) {
   //   console.log(dataTemp.data.values.temperature);
 }
 
+function checkWeather(weatherCode) {
+  switch (weatherCode) {
+    case 1000:
+    case 1100:
+      imageWeather.src = `./images/clear.png`;
+      break;
+    case 1101:
+    case 1102:
+    case 1001:
+      imageWeather.src = `./images/clouds.png`;
+      break;
+    case 4000:
+      imageWeather.src = `./images/drizzle.png`;
+      break;
+    case 4001:
+    case 4200:
+    case 4201:
+      imageWeather.src = `./images/rain.png`;
+      break;
+    default:
+      imageWeather.src = `./images/clear.png`;
+      break;
+  }
+}
+
 document.querySelector(".check").addEventListener("click", function () {
   const city = document.querySelector(".cityName").value;
   //   console.log(city);
   const res = getData(city);
   res.then((res) => {
-    document.querySelector(".city").textContent = city.toUpperCase();
+    const cityyy = res.location.name.split(",");
+    document.querySelector(".city").textContent = cityyy[0];
     document.querySelector(".temperature").textContent =
       res.data.values.temperature;
     document.querySelector(".humidity-percent").textContent =
       res.data.values.humidity;
     document.querySelector(".wind").textContent = res.data.values.windSpeed;
-    console.log(res.data.values.temperature);
-    console.log(res);
+    // console.log(res.data.values.temperature);
+    checkWeather(res.data.values.weatherCode);
   });
 });
